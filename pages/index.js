f5cd38367aad11ea22096b2b1efbc90be560e374
You Song Hou <yousonghou@gmail.com>
2022-06-12 17:49:50 -0400

    connect the front end to contract using web3

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,93 @@
-import React from 'react';
-import { useContractKit } from '@celo-tools/use-contractkit';
-import { ContractKitProvider } from '@celo-tools/use-contractkit';
-import '@celo-tools/use-contractkit/lib/styles.css';
+import React, { useState } from 'react';
+import Web3 from 'web3';
+
+const abi = [
+  {
+      "inputs": [
+          {
+              "internalType": "string",
+              "name": "_greeting",
+              "type": "string"
+          }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+  },
+  {
+      "inputs": [],
+      "name": "greet",
+      "outputs": [
+          {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "string",
+              "name": "_greeting",
+              "type": "string"
+          }
+      ],
+      "name": "setGreeting",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+  }
+];
+
+const address = "0xB13318E45F727D6FCE72502106b63BAae1bFBBc3";
 
 function App() {
-  const { address, connect } = useContractKit()
+  const [account, setAccount] = useState('');
+  const [gContract, setGContract] = useState(null);
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+
+      await window.ethereum.enable();
+      await loadBlockchain();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+      await loadBlockchain();
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  const loadBlockchain = async () => {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+
+    const contract = await new web3.eth.Contract(abi, address);
+    setGContract(contract);
+    const greet = await contract.methods.greet().call();
+    console.log(greet);
+  }
+
+  const setGreet = async () => {
+    const res = await gContract.methods.setGreeting("Hi").send({ from: account });
+    console.log(res);
+  }
 
   return (
     <main>
       <h1>Celo Voting DApp</h1>
       <p>{address}</p>
-      <button onClick={connect}>Click here to connect your wallet</button>
+      <button onClick={connectWallet}>Click here to connect your wallet</button>
+      <button onClick={setGreet}>Set Greet</button>
     </main>
   )
 }
 
-function WrappedApp() {
-  return (
-    <ContractKitProvider
-      dapp={{
-          name: "My awesome dApp",
-          description: "My awesome description",
-          url: "https://example.com",
-        }}
-    >
-      <App />
-    </ContractKitProvider>
-  );
-}
-export default WrappedApp;
+export default App;
