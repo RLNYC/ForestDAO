d8beb59d8ad36af5842b0c9a425eb0104d8462b0
You Song Hou <yousonghou@gmail.com>
2022-06-15 18:26:02 -0400

    add DAO feature to the contract

--- contracts/ForestDAO.sol
+++ contracts/ForestDAO.sol
@@ -6,34 +6,84 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ForestDAO is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter public treeIds;
-    mapping(uint => Tree) public listoftrees;
+  using Counters for Counters.Counter;
+  Counters.Counter public treeIds;
+  mapping(uint => Tree) public listoftrees;
+  uint proposalCount = 0;
+  mapping(uint => TreeProposal) public listofProposal;
 
-    struct Tree {
-      uint tokenId;
-      address owner;
-    }
+  struct Tree {
+    uint tokenId;
+    address owner;
+  }
 
-    event TreeCreated (
-      uint tokenId,
-      address owner
-    );
+  event TreeCreated (
+    uint256 tokenId,
+    address owner
+  );
 
-    constructor() ERC721("COTree NFT", "COT") {
-    }
+  struct TreeProposal {
+    uint id;
+    string detail;
+    uint yes;
+    uint no;
+    uint date;
+    address owner;
+  }
 
-    function mintTree() public payable returns (uint) {
-      treeIds.increment();
-      uint256 newTreeId = treeIds.current();
+  event ProposalCreated (
+    uint id,
+    string details,
+    uint yes,
+    uint no,
+    uint date,
+    address owner
+  );
 
-      _mint(msg.sender, newTreeId);
+  event ProposalVote (
+    uint id,
+    string result,
+    address from
+  );
 
-      listoftrees[newTreeId] = Tree(newTreeId, msg.sender);
-      emit TreeCreated(newTreeId, msg.sender);
+  constructor() ERC721("COTree NFT", "COT") {
+  }
 
-      return newTreeId;
-    }
+  function mintTree() public payable returns (uint) {
+    treeIds.increment();
+    uint256 newTreeId = treeIds.current();
+
+    _mint(msg.sender, newTreeId);
+
+    listoftrees[newTreeId] = Tree(newTreeId, msg.sender);
+    emit TreeCreated(newTreeId, msg.sender);
+
+    return newTreeId;
+  }
+
+  function createProposal(string memory _detail) public returns (uint) {
+    proposalCount++;
+    listofProposal[proposalCount] = TreeProposal(proposalCount, _detail, 0, 0, block.timestamp, msg.sender);
+    emit ProposalCreated(proposalCount, _detail, 0, 0, block.timestamp, msg.sender);
+
+    return proposalCount;
+  }
+
+  function voteYes(uint _proposalId) external {
+    TreeProposal memory _treeProposal = listofProposal[_proposalId];
+    _treeProposal.yes += 1;
+    listofProposal[_proposalId] = _treeProposal;
+
+    emit ProposalVote(_proposalId, "Yes", msg.sender);
+  }
+
+  function voteNo(uint _proposalId) external {
+    TreeProposal memory _treeProposal = listofProposal[_proposalId];
+    _treeProposal.no += 1;
+    listofProposal[_proposalId] = _treeProposal;
+
+    emit ProposalVote(_proposalId, "No", msg.sender);
+  }
 
   function fetchUserTreeNFTs(address _userAddress) public view returns (Tree[] memory) {
     uint totalNFTCount = treeIds.current();
